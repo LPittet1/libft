b96d76f953a27f36aab77aba73e9d26fe9cb1639
LPittet1 <pittet82@gmail.com>
2024-10-02 14:02:48 +0200

    updated library not valid temp for tests

--- libft.h
+++ libft.h
@@ -6,15 +6,17 @@
 /*   By: lpittet <lpittet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 10:01:27 by lpittet           #+#    #+#             */
-/*   Updated: 2024/10/02 10:06:22 by lpittet          ###   ########.fr       */
+/*   Updated: 2024/10/02 11:21:48 by lpittet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-#include <stdlib.h>
-#include <unistd.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+void	*ft_memset(void *s, int c, size_t n);
 
 //to remove before submit for test
 #include <stdio.h>
