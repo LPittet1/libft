fd9188f8ca60e47551f5be4e9671fd6605f02458
Loic Pittet <lpittet@c2r5s3.42lausanne.ch>
2024-10-04 10:36:32 +0200

    split correct

--- ft_split.c
+++ ft_split.c
@@ -6,18 +6,19 @@
 /*   By: lpittet <lpittet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 13:27:59 by lpittet           #+#    #+#             */
-/*   Updated: 2024/10/03 14:40:18 by lpittet          ###   ########.fr       */
+/*   Updated: 2024/10/04 10:35:02 by lpittet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
- static int	count_words(char const *s, char c)
+static size_t	count_words(char const *s, char c)
 {
 	int	i;
-	int	count;
+	size_t	count;
 
 	count = 0;
+	i = 0;
 	while (s[i])
 	{
 		while (s[i] && s[i] != c)
@@ -32,54 +33,60 @@
 	return (count);
 }
 
-char	**ft_split(char const *s, char c)
+static	char	*ft_word(char const *s, int start, int end)
 {
-	int		count;
-	char	**ret;
+	char	*str;
 	int		i;
-	int		start;
-	int		ccount;
 
 	i = 0;
-	ccount = 0;
-	printf("a\n");
-	count = count_words(s, c);
-	printf("a\n");
-	ret = (char**)malloc(sizeof(char*) * (count + 1));
-	if (ret == NULL)
-		return(NULL);
+	str = malloc(sizeof(char) * (end - start));
+	if (str == NULL)
+		return (NULL);
+	while (start < end)
+	{
+		str[i] = s[start];
+		i++;
+		start++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**tab;
+	int		i;
+	int		start;
+	int		iword;
+
+	i = 0;
+	iword = 0;
+	tab = malloc(sizeof(char *) * (count_words(s, c) + 1));
+	if (tab == NULL)
+		return (NULL);
 	while (s[i])
 	{
 		while (s[i] && s[i] == c)
 			i++;
-		printf("a\n");
 		start = i;
 		while (s[i] && s[i] != c)
 			i++;
-		if (start < i)
+		if (i > start)
 		{
-			ret[ccount] = ft_substr(s, start, i - start + 1);
-			ccount++;
+			tab[iword] = ft_word(s, start, i);
+			iword++;
 		}
 	}
-	ret[ccount] = 0;
-	return (ret);
+	tab[iword] = 0;
+	return (tab);
 }
 
 int main()
 {
+	char *s =  ",,,,,un,petit,,,,test,pour,voir,si,ca,fonctionne,,,,,";
+	char c =  ',';
 	int i = 0;
-	
-	char *str = "bachbjla ajl";
-	
-	char sep = 'a';
-	
-	printf("%d", count_words(str, sep));
-	printf("a\n");
-	char **tab;
-	
-	tab = ft_split(str, sep);
-	
+	char **tab = ft_split(s, c);
 	while (tab[i])
 	{
 		printf("%s\n", tab[i]);
