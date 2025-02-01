605a729a0b345ceb0537a4ec92384456c4985fe9
Loic Pittet <lpittet@c2r5s3.42lausanne.ch>
2025-02-01 12:49:03 +0100

    fixed strlen to work with empty srings

--- srcs/ft_strlen.c
+++ srcs/ft_strlen.c
@@ -6,17 +6,20 @@
 /*   By: lpittet <lpittet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 11:37:58 by lpittet           #+#    #+#             */
-/*   Updated: 2024/11/12 08:33:01 by lpittet          ###   ########.fr       */
+/*   Updated: 2025/02/01 12:47:40 by lpittet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
+#include <stdio.h>
 
 size_t	ft_strlen(const char *str)
 {
 	int	i;
 
 	i = 0;
+	if (!str)
+		return (0);
 	while (str[i])
 	{
 		i++;
